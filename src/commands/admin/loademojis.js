0c681d35688df1d404897affd6b628e5a3fa99b9
Linus Graaskov <linusgraaskov@gmail.com>
2023-03-16 20:20:33 +0100

    fixed bug where loading files would crash due to mac .DS_Store file. added a command to load and format all emojis from current guild.

---
+++ src/commands/admin/loademojis.js
@@ -0,0 +1,41 @@
+export default {
+  name: "loademojis",
+  aliases: ["le"],
+  arguments: "",
+  description: "Load all emojis.",
+  category: "Admin",
+  async execute(message, args, config, player, server) {
+    const guilds = client.guilds.cache.map((x) => {
+      return { id: x.id, name: x.name, emojis: x.emojis };
+    });
+
+    for (let guild of guilds) {
+      guild.emojis = (await guild.emojis.fetch()).map((x) => {
+        return { id: x.id, name: x.name, animated: x.animated };
+      });
+    }
+
+    // const rejoice_testing = guilds.find((x) => x.name == "rejoice testing");
+    const guild = guilds.find((x) => x.id == message.guild.id);
+    if (!guild) return;
+
+    let emojiList = "";
+    for (const emoji of guild.emojis) {
+      let animated = emoji.animated ? "a" : "";
+      let emojiFormat = `<${animated}:${emoji.name}:${emoji.id}>`;
+      emojiList += `\n${emojiFormat} \`${emojiFormat}\``;
+      //   emojiList += emojiFormat;
+    }
+
+    if (emojiList.length > 4000)
+      emojiList =
+        emojiList.slice(0, 4000) + "...\nToo many emojis for one embed :(";
+
+    const embed = {
+      color: config.botColor,
+      description: emojiList,
+    };
+
+    game.sendEmbed(message, embed);
+  },
+};
