c058724822a20368677461c2bfc95a375f0b4c30
Linus Graaskov <linusgraaskov@gmail.com>
2024-01-21 00:13:06 +0100

    more clothing options

--- src/functions/player/getCosmetics.ts
+++ src/functions/player/getCosmetics.ts
@@ -5,9 +5,9 @@ import fs from "fs";
 export default (async function () {
   const cosmetics = {
     hair: ["nothing", "cool", "long", "pigtails", "poofy", "short", "very_short"],
-    torso: ["nothing", "bra", "crop_top", "long_shirt", "plain_shirt", "rough_shirt"],
-    legs: ["nothing", "shorts", "skirt", "trousers", "underwear"],
-    feet: ["nothing", "shoes"],
+    torso: ["nothing", "bra", "crop_top", "long_shirt", "plain_shirt", "rough_shirt", "fishnets"],
+    legs: ["nothing", "shorts", "skirt", "trousers", "underwear", "fishnets"],
+    feet: ["nothing", "shoes", "heels", "high_boots"],
   };
 
   return cosmetics;
